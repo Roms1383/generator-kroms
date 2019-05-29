d9e1b277db6b195c0b0662134918a9f440b52c04
roms13 <romain.kelifa@gmail.com>
2019-05-29 19:40:07 +0700

    :green_heart: Fix unit-test
    Fix unit-test where empty repository required

--- utils/inspect/files/index.test.js
+++ utils/inspect/files/index.test.js
@@ -1,8 +1,11 @@
+const fs = require('fs')
 const path = require('path')
 const files = require('.')
 describe('files', () => {
   it('where none', () => {
+    fs.mkdirSync(path.resolve(__dirname, 'empty'))
     expect(files(path.resolve(__dirname, 'empty'))).toHaveLength(0)
+    fs.rmdirSync(path.resolve(__dirname, 'empty'))
   })
   it('where some', () => {
     expect(files(path.resolve(__dirname)).length).toBeGreaterThan(0)
