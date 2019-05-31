45002bcaec91d49011ee30f08f88d0cb6144c286
roms13 <romain.kelifa@gmail.com>
2019-05-31 14:31:28 +0700

    :zap: Improve logic for arrayify

--- utils/arrayify/index.js
+++ utils/arrayify/index.js
@@ -1,8 +1,6 @@
 const R = require('ramda')
-module.exports = variable => R.isNil(variable)
+module.exports = variable => R.isNil(variable) || Array.isArray(variable)
 ? variable
-: Array.isArray(variable)
-  ? variable
-  : typeof variable === 'string' && variable.indexOf('.')
-    ? variable.split('.')
-    : [variable]
+: typeof variable === 'string' && variable.indexOf('.')
+  ? variable.split('.')
+  : [variable]
