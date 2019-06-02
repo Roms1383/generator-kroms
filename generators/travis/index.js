25cc76d6cfc267ef0ccde63d710460539bd4845c
roms13 <romain.kelifa@gmail.com>
2019-06-03 04:02:25 +0700

    :sparkles: Add support for disabling coverage in CI
    Disable coverage in Travis config by explictly setting coverage to false in .yo-rc.json

--- generators/travis/index.js
+++ generators/travis/index.js
@@ -1,3 +1,4 @@
+const R = require('ramda')
 const Generator = require('../../utils/generator')
 module.exports = class extends Generator {
   initializing () {
@@ -7,7 +8,8 @@ module.exports = class extends Generator {
     const options = {
       npm: this.npm.latest('npm'),
       yarn: this.npm.latest('yarn'),
-      tests: this.tested
+      tests: this.tested,
+      coverage: R.isNil(this.config.get('coverage')) || this.config.get('coverage')
     }
     this.fs.copyTpl(this.templatePath('node'), this.destinationPath('.travis.yml'), options)
   }
