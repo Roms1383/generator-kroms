3cf5b27ea7d3c4c330856cba4b3528690c4ac1ba
roms13 <romain.kelifa@gmail.com>
2019-06-04 03:01:49 +0700

    :zap: Use fixed version of readdirp

--- utils/inspect/index.js
+++ utils/inspect/index.js
@@ -1,19 +1,12 @@
 const { readdirSync } = require('fs')
-const readdirp = require('readdirp')
-const fileFilter = ({ path }) => path.indexOf('.git') !== 0
-&& path.indexOf('node_modules') !== 0
-&& path.indexOf('coverage') !== 0
-&& path.indexOf('my-tests') !== 0
+const readdirp = require('readdirp').promise
 const file = async (filename, root = '.', recursive = false) => recursive
-? new Promise((resolve) => {
-  readdirp(root, { fileFilter, type: 'files_directories' })
-  .on('data', ({ basename }) => { if (basename === filename) return resolve(true) })
-  .on('end', () => resolve(false))
-})
+? readdirp(root, { fileFilter: filename, type: 'files_directories' })
+.then(results => results.length > 0)
 : readdirSync(root).find(basename => basename === filename) !== undefined
-const files = async (extension, root = '.') => new Promise((resolve) => {
-  readdirp(root, { fileFilter })
-  .on('data', ({ basename }) => { if (basename.match(`.${extension}$`) !== null) return resolve(true) })
-  .on('end', () => resolve(false))
+const files = async (extension, root = '.') => readdirp(root, {
+  fileFilter: `*.${extension}`,
+  directoryFilter: ['!.git', '!node_modules', '!coverage', '!my-tests']
 })
+.then(results => results.length > 0)
 module.exports = { file, files }
